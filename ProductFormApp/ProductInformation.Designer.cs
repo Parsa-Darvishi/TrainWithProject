0ce4dd6213e9ca47ac23922912a2366df401ff21
Parham Darvishi <darvishiparham14@gmail.com>
2025-03-07 13:47:54 +0330

    Change ON Items

--- ProductFormApp/ProductInformation.Designer.cs
+++ ProductFormApp/ProductInformation.Designer.cs
@@ -31,43 +31,83 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            nameTextBox = new TextBox();
+            priceTextBox = new TextBox();
+            IdTextBox = new TextBox();
+            addProductButton = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(22, 8);
+            label1.Location = new Point(25, 11);
             label1.Name = "label1";
-            label1.Size = new Size(81, 15);
+            label1.Size = new Size(49, 20);
             label1.TabIndex = 0;
-            label1.Text = "ProductName";
+            label1.Text = "Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(22, 54);
+            label2.Location = new Point(25, 48);
             label2.Name = "label2";
-            label2.Size = new Size(75, 15);
+            label2.Size = new Size(41, 20);
             label2.TabIndex = 1;
-            label2.Text = "ProductPrice";
+            label2.Text = "Price";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(22, 96);
+            label3.Location = new Point(25, 90);
             label3.Name = "label3";
-            label3.Size = new Size(59, 15);
+            label3.Size = new Size(22, 20);
             label3.TabIndex = 2;
-            label3.Text = "ProductId";
+            label3.Text = "Id";
+            // 
+            // nameTextBox
+            // 
+            nameTextBox.Location = new Point(131, 8);
+            nameTextBox.Name = "nameTextBox";
+            nameTextBox.Size = new Size(220, 27);
+            nameTextBox.TabIndex = 3;
+            // 
+            // priceTextBox
+            // 
+            priceTextBox.Location = new Point(131, 45);
+            priceTextBox.Name = "priceTextBox";
+            priceTextBox.Size = new Size(220, 27);
+            priceTextBox.TabIndex = 4;
+            // 
+            // IdTextBox
+            // 
+            IdTextBox.Location = new Point(131, 87);
+            IdTextBox.Name = "IdTextBox";
+            IdTextBox.Size = new Size(220, 27);
+            IdTextBox.TabIndex = 5;
+            // 
+            // addProductButton
+            // 
+            addProductButton.Location = new Point(25, 120);
+            addProductButton.Name = "addProductButton";
+            addProductButton.Size = new Size(326, 38);
+            addProductButton.TabIndex = 6;
+            addProductButton.Text = "Add Product";
+            addProductButton.UseVisualStyleBackColor = true;
+            addProductButton.Click += addProductButton_Click;
             // 
             // ProductInformation
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(addProductButton);
+            Controls.Add(IdTextBox);
+            Controls.Add(priceTextBox);
+            Controls.Add(nameTextBox);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "ProductInformation";
             Text = "Form1";
             Load += Form1_Load;
@@ -80,5 +120,9 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private TextBox nameTextBox;
+        private TextBox priceTextBox;
+        private TextBox IdTextBox;
+        private Button addProductButton;
     }
 }
--- ProductFormApp/ProductInformation.cs
+++ ProductFormApp/ProductInformation.cs
@@ -1,15 +1,38 @@
+using TrainWith.Businesses;
+using TrainWith.Entity;
+
 namespace ProductFormApp
 {
     public partial class ProductInformation : Form
     {
+        ProductBusiness productBusiness = null;
         public ProductInformation()
         {
             InitializeComponent();
+            productBusiness = new ProductBusiness();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void addProductButton_Click(object sender, EventArgs e)
+        {
+            Product item = new Product()
+            {
+                Name = nameTextBox.Text,
+                Id = int.Parse(IdTextBox.Text),
+                Price = decimal.Parse(priceTextBox.Text),
+            };
+            productBusiness.Add(item: item);
+            Test(y: 2,z: 4);
+        }
+
+
+        public void Test(int y,int z)
+        {
+            int x = y;
         }
     }
 }
